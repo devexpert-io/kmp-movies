edb9b295b0d208aaebe495e8eede2752f66978ce
antoniolg <antonio@devexperto.com>
2024-06-04 13:51:59 +0200

    Transparent navigation bars

--- composeApp/src/androidMain/kotlin/io/devexpert/kmpmovies/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/devexpert/kmpmovies/MainActivity.kt
@@ -1,17 +1,40 @@
 package io.devexpert.kmpmovies
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
+            EnableTransparentStatusBar()
             App()
+        }
+    }
+}
+
+@Composable
+private fun EnableTransparentStatusBar() {
+    val view = LocalView.current
+    val darkTheme = isSystemInDarkTheme()
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars =
+                !darkTheme
         }
     }
 }
