59cbce1c6e9f7da5f109d899e0151dbebe4c70a8
antoniolg <antonio@devexperto.com>
2024-06-04 13:51:16 +0200

    Build DetailScreen

---
+++ composeApp/src/commonMain/kotlin/io/devexpert/kmpmovies/ui/screens/home/DetailScreen.kt
@@ -0,0 +1,71 @@
+package io.devexpert.kmpmovies.ui.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import io.devexpert.kmpmovies.movies
+import io.devexpert.kmpmovies.ui.screens.Screen
+import kmpmovies.composeapp.generated.resources.Res
+import kmpmovies.composeapp.generated.resources.back
+import org.jetbrains.compose.resources.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen() {
+    val movie = movies[0]
+    Screen {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = movie.title) },
+                    navigationIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                contentDescription = stringResource(Res.string.back)
+                            )
+                        }
+                    }
+                )
+            },
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .padding(padding)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                AsyncImage(
+                    model = movie.poster,
+                    contentDescription = movie.title,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16 / 9f)
+                )
+
+                Text(
+                    text = movie.title,
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+        }
+    }
+}
