3f11cc3e1357cc00655e31ef6f8457b688755e8b
antoniolg <antonio@devexperto.com>
2024-06-10 12:48:08 +0200

    Calculate region for iOS

--- composeApp/src/iosMain/kotlin/io/devexpert/kmpmovies/data/IosRegionDataSource.kt
+++ composeApp/src/iosMain/kotlin/io/devexpert/kmpmovies/data/IosRegionDataSource.kt
@@ -1,7 +1,58 @@
 package io.devexpert.kmpmovies.data
 
-class IosRegionDataSource: RegionDataSource {
+import platform.CoreLocation.CLLocation
+import platform.CoreLocation.CLLocationManager
+import platform.Foundation.NSError
+import platform.darwin.NSObject
+import kotlinx.coroutines.suspendCancellableCoroutine
+import platform.CoreLocation.CLGeocoder
+import platform.CoreLocation.CLLocationManagerDelegateProtocol
+import platform.CoreLocation.CLPlacemark
+import kotlin.coroutines.resume
+
+class IosRegionDataSource : RegionDataSource {
+
+    private val locationManager = CLLocationManager()
+
     override suspend fun fetchRegion(): String {
-        TODO("Not yet implemented")
+        return getCurrentLocation()?.toRegion() ?: DEFAULT_REGION
+    }
+
+    private suspend fun getCurrentLocation(): CLLocation? {
+        return suspendCancellableCoroutine { continuation ->
+            locationManager.delegate = object : NSObject(), CLLocationManagerDelegateProtocol {
+                override fun locationManager(
+                    manager: CLLocationManager,
+                    didUpdateLocations: List<*>
+                ) {
+                    val location = didUpdateLocations.lastOrNull() as? CLLocation
+                    locationManager.stopUpdatingLocation()
+                    continuation.resume(location)
+                }
+
+                override fun locationManager(
+                    manager: CLLocationManager,
+                    didFailWithError: NSError
+                ) {
+                    continuation.resume(null)
+                }
+            }
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    private suspend fun CLLocation.toRegion(): String {
+        return suspendCancellableCoroutine { continuation ->
+            val geocoder = CLGeocoder()
+            geocoder.reverseGeocodeLocation(this) { placemarks, error ->
+                if (error != null || placemarks == null) {
+                    continuation.resume(DEFAULT_REGION)
+                } else {
+                    val countryCode = placemarks.firstOrNull()?.let { (it as CLPlacemark).ISOcountryCode }
+                    continuation.resume(countryCode ?: DEFAULT_REGION)
+                }
+            }
+        }
     }
 }
